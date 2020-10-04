eb5f1c83bd180d3e18d5d94b1f5361407651aa88
Oussama BEN MAHMOUD <bm.oussema@gmail.com>
2020-10-04 15:17:02 +0200

    TRAIN-6 used built-in types, union types and activated stringNullChecks compiler option

--- getting-started/app/app.ts
+++ getting-started/app/app.ts
@@ -1,7 +1,14 @@
 function startGame() {
     // starting new game
+    let playerName: string = 'Alice';
+    logPlayer(playerName);
+
     var messageElement = document.getElementById('messages');
     messageElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+
+function logPlayer(name) {
+    console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
