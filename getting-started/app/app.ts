b59bef6e82a46cb6de53b228d946fdbc6ebe1cee
Oussama BEN MAHMOUD <bm.oussema@gmail.com>
2020-10-04 21:15:43 +0200

    TRAIN-6 used function types

--- getting-started/app/app.ts
+++ getting-started/app/app.ts
@@ -4,6 +4,7 @@ function startGame() {
     logPlayer(playerName);
 
     postScore(80, playerName);
+    postScore(-5, playerName);
 }
 
 function logPlayer(name: string = 'Multimath Player'): void {
@@ -20,11 +21,25 @@ function getInputValue(elementId: string): string | undefined {
 }
 
 function postScore(score: number, playerName: string = 'Multimath Player'): void {
+
+    let logger: (value: string) => void;
+
+    if(score < 0) {
+        logger = logError;
+    } else {
+        logger = logMessage;
+    }
+
     const scoreElement: HTMLElement | null = document.getElementById('postedScores');
     scoreElement!.innerText = `${score} - ${playerName}`;
+
+    logger(`Score ${score}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
 
 const logMessage = (message:string) => console.log(message);
-logMessage('Welcome to MultiMath!');
+
+function logError(err: string): void {
+    console.error(err);
+}
