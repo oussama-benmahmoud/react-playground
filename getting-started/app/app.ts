32fb237f5a55cd1c77802ed8a5a160557f2614aa
Oussama BEN MAHMOUD <bm.oussema@gmail.com>
2020-10-04 21:06:26 +0200

    TRAIN-6 added type annotation and activated noImplicitAny compiler option

--- getting-started/app/app.ts
+++ getting-started/app/app.ts
@@ -1,14 +1,27 @@
 function startGame() {
     // starting new game
-    let playerName: string = 'Alice';
+    let playerName: string | undefined = getInputValue('playername');
     logPlayer(playerName);
 
-    var messageElement = document.getElementById('messages');
-    messageElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+    postScore(80, playerName);
 }
 
-function logPlayer(name) {
+function logPlayer(name: string = 'Multimath Player'): void {
     console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementId: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
+    if(inputElement.value === '') {
+        return undefined;
+    } else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score: number, playerName: string = 'Multimath Player'): void {
+    const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
